04acdac63d445e89c1dc6095b6377004176edae1
Yongjin <hi653@naver.com>
2024-07-23 19:52:55 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240723/트로미노/tromino.cpp
+++ 240723/트로미노/tromino.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int n,m,res;
 int arr[202][202];
 
-void func(int y, int x){
+void func(int x, int y){
     res = max(res, arr[y][x] + max(arr[y][x+1]+ max(arr[y+1][x+1], arr[y][x+2]),arr[y+1][x]+max(arr[y+2][x],arr[y+1][x+1])));
     res = max(res,arr[y][x]+arr[y+1][x]+arr[y][x+1]);
 }
