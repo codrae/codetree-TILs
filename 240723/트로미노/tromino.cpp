5f82745e25fca2f1aad4862b2f30e412f81a2168
Yongjin <hi653@naver.com>
2024-07-23 20:12:03 +0900

    Title: 트로미노, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240723/트로미노/tromino.cpp
+++ 240723/트로미노/tromino.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
+#include <algorithm> // For the max function
 using namespace std;
 
-int n,m,res;
+int n, m, res;
 int arr[202][202];
-int a,b,c,d;
 
-void func(int c, int r){
-   a = arr[c+1][r] + max(arr[c+1][r+1], arr[c+2][r]);
-   b = arr[c-1][r] + max(arr[c-1][r-1], arr[c-1][r+1]);
-   c = arr[c][r+1] + max(arr[c][r+2],arr[c+1][r+1]);  
-   a = max(a,b);
-   a = max(a,c);
-   res = max(a+arr[c][r],res);
-   res = max(res, arr[c][r]+arr[c+1][r]+arr[c][r+1]);   
+void func(int col, int row) {
+   int valA = arr[col + 1][row] + max(arr[col + 1][row + 1], arr[col + 2][row]);
+   int valB = arr[col - 1][row] + max(arr[col - 1][row - 1], arr[col - 1][row + 1]);
+   int valC = arr[col][row + 1] + max(arr[col][row + 2], arr[col + 1][row + 1]);  
+   
+   int maxVal = max({valA, valB, valC});
+   res = max(maxVal + arr[col][row], res);
+   res = max(res, arr[col][row] + arr[col + 1][row] + arr[col][row + 1]);   
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     cin >> n >> m;
     for(int i = 1; i <= n; i++){
-        for(int j = 1; j <=m; j++){
+        for(int j = 1; j <= m; j++){
             cin >> arr[i][j];
         }
     }
 
-
     for(int i = 1; i <= n; i++){
-        for(int j = 1; j <=m; j++){
-            func(i,j);
+        for(int j = 1; j <= m; j++){
+            func(i, j);
         }
     }
 
     cout << res;
-
-
     return 0;
 }
