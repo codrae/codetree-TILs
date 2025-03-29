8d90a8166ff0bc9c6f2360be9539a24342f8d6ab
KIMYONGJIN <hi653@naver.com>
2025-03-29 11:48:54 +0900

    Title: 두 정수에 대한 연산값 2, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250329/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 250329/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -3,7 +3,8 @@ a, b = map(int, input().split())
 # Please write your code here.
 
 def cal(a,b):
-    if(a>b) : a*=2 b+=10
-    else : a+=10 b*=2
+    if(a>b) : a*=2; b+=10
+    else : a+=10; b*=2
     return a,b
-print(cal(a,b))
+
+print(*cal(a,b))
